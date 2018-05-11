639ac520d46cd0a2c80355e83ab7deb8235075ef
garrett-vangilder <garrett.vangilder@gmail.com>
2018-05-10 21:13:35 -0400

    adds initial infrastructure

---
+++ src/components/chart.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Chart extends Component {
+  render() {
+    return <div>This is the Chart</div>;
+  }
+}
+
+export default Chart;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reduxThunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Router, Route, browserHistory } from 'react-router';
+import rootReducer from './reducers'
+
+const createStoreWithMiddleware = compose(applyMiddleware(reduxThunk)(createStore));
+const store = createStoreWithMiddleware(rootReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
