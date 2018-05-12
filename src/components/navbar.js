a48e846d97a6017287d0530b498bc3699df05827
garrett-vangilder <garrett.vangilder@gmail.com>
2018-05-12 09:59:59 -0400

    adds navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Tabs, { Tab } from 'material-ui/Tabs';
+
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
 
 class Navbar extends Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+
   render() {
-    return <div>This is the Navbar</div>;
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Tabs value={value} onChange={this.handleChange}>
+            <Tab label="Salary By Position" />
+            <Tab label="Salary By Team" />
+          </Tabs>
+        </AppBar>
+      </div>
+    );
   }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
